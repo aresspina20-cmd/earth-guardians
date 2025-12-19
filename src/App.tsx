dce69a29353f83549e8e01a35c2f87f9c9b292e8
aresspina20-cmd <ares.spina20@istitutotecnicomarconipilla.edu.it>
2025-12-19 13:35:06 +0100

    Remove QueryClientProvider and update Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Antropocene from "./pages/Antropocene";
@@ -14,29 +10,21 @@ import EconomiaCircolare from "./pages/EconomiaCircolare";
 import Crediti from "./pages/Crediti";
 import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/antropocene" element={<Antropocene />} />
-          <Route path="/crisi-climatica" element={<CrisiClimatica />} />
-          <Route path="/agenda-2030" element={<Agenda2030 />} />
-          <Route path="/risorse-energetiche" element={<RisorseEnergetiche />} />
-          <Route path="/acqua" element={<Acqua />} />
-          <Route path="/rifiuti" element={<Rifiuti />} />
-          <Route path="/economia-circolare" element={<EconomiaCircolare />} />
-          <Route path="/crediti" element={<Crediti />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <BrowserRouter basename="/earth-guardians">
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/antropocene" element={<Antropocene />} />
+      <Route path="/crisi-climatica" element={<CrisiClimatica />} />
+      <Route path="/agenda-2030" element={<Agenda2030 />} />
+      <Route path="/risorse-energetiche" element={<RisorseEnergetiche />} />
+      <Route path="/acqua" element={<Acqua />} />
+      <Route path="/rifiuti" element={<Rifiuti />} />
+      <Route path="/economia-circolare" element={<EconomiaCircolare />} />
+      <Route path="/crediti" element={<Crediti />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
