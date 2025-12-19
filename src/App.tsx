ebce40690d93ef743ccfe0ea169a4dabe20a719e
aresspina20-cmd <ares.spina20@istitutotecnicomarconipilla.edu.it>
2025-12-19 18:44:32 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import Antropocene from "./pages/Antropocene";
-import CrisiClimatica from "./pages/CrisiClimatica";
-import Agenda2030 from "./pages/Agenda2030";
-import RisorseEnergetiche from "./pages/RisorseEnergetiche";
-import Acqua from "./pages/Acqua";
-import Rifiuti from "./pages/Rifiuti";
-import EconomiaCircolare from "./pages/EconomiaCircolare";
-import Crediti from "./pages/Crediti";
-import NotFound from "./pages/NotFound";
+import Index from './pages/Index';
 
-const App = () => (
-  <BrowserRouter basename="/earth-guardians">
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/antropocene" element={<Antropocene />} />
-      <Route path="/crisi-climatica" element={<CrisiClimatica />} />
-      <Route path="/agenda-2030" element={<Agenda2030 />} />
-      <Route path="/risorse-energetiche" element={<RisorseEnergetiche />} />
-      <Route path="/acqua" element={<Acqua />} />
-      <Route path="/rifiuti" element={<Rifiuti />} />
-      <Route path="/economia-circolare" element={<EconomiaCircolare />} />
-      <Route path="/crediti" element={<Crediti />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
-);
-
-export default App;
+export default function App() {
+  return <Index />;
+}
